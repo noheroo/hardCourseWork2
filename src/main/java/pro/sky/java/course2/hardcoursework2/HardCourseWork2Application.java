412d64c6798bc9e909bf2a1c76163453f59fcb77
nohero_ <serega1552@yandex.ru>
2022-06-29 17:10:26 +0400

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/hardcoursework2/HardCourseWork2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hardcoursework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardCourseWork2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardCourseWork2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/hardcoursework2/HardCourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hardcoursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardCourseWork2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
